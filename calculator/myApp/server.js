324133aa1490847861c4b10aa90516827cc7f93a
Ali Ashraf <adaydreamer10@gmail.com>
2022-06-03 17:32:46 +0200

    endpoint /multiply

--- calculator/myApp/server.js
+++ calculator/myApp/server.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express")
 const app = express()
 
@@ -11,5 +12,14 @@ app.get("/add", (request, respone) => {
     let sum = num1 + num2
     respone.send(`The sum of both num is ${sum}`)
 });
+
+
+app.get("/multiply" , (request, response) => {
+    let num1 = parseInt(request.query.num1)
+    let num2 = parseInt(request.query.num2)
+    let multiply = num1 * num2
+    response.send(`The multiply of you your numbers is ${multiply}`)
+})
+
 
 app.listen(4000, () => console.log("Serever is up and runing"));
