f041b41fd3d72b0b06cda1e998281331003ba4db
Ali Ashraf <adaydreamer10@gmail.com>
2022-06-03 18:05:39 +0200

    endpoint /divide

--- calculator/myApp/server.js
+++ calculator/myApp/server.js
@@ -6,11 +6,11 @@ app.get("/", (request, response) => {
     response.send("Hello world");
 });
 
-app.get("/add", (request, respone) => {
+app.get("/add", (request, response) => {
     let num1 = parseInt(request.query.num1)
     let num2 = parseInt(request.query.num2)
     let sum = num1 + num2
-    respone.send(`The sum of both num is ${sum}`)
+    response.send(`The sum of both num is ${sum}`)
 });
 
 
@@ -20,6 +20,15 @@ app.get("/multiply" , (request, response) => {
     let multiply = num1 * num2
     response.send(`The multiply of you your numbers is ${multiply}`)
 })
+
+
+app.get("/divide" , (request, response) => {
+    let num1 = parseInt(request.query.num1)
+    let num2 = parseInt(request.query.num2)
+    let multiply = num1 / num2
+    response.send(`The multiply of you your numbers is ${multiply}`)
+})
+
 
 
 app.listen(4000, () => console.log("Serever is up and runing"));
