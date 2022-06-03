04c9909539adfc933a55a223880bba0f7b564cef
Ali Ashraf <adaydreamer10@gmail.com>
2022-06-03 18:26:33 +0200

    Making endpoints using endpoint params add/:value1/:value2

--- calculator/myApp/server.js
+++ calculator/myApp/server.js
@@ -2,10 +2,15 @@ const { application } = require("express");
 const express = require("express")
 const app = express()
 
+
+// the first basic endpoint
 app.get("/", (request, response) => {
-    response.send("Hello world");
+    response.send("App is alive and running");
 });
 
+
+// STEP 1 make endpoints using Query Params
+// the end point which takes two number and do addition of two numbers.
 app.get("/add", (request, response) => {
     let num1 = parseInt(request.query.num1)
     let num2 = parseInt(request.query.num2)
@@ -13,7 +18,7 @@ app.get("/add", (request, response) => {
     response.send(`The sum of both num is ${sum}`)
 });
 
-
+// the end point which takes two number and do multiplication.
 app.get("/multiply" , (request, response) => {
     let num1 = parseInt(request.query.num1)
     let num2 = parseInt(request.query.num2)
@@ -22,6 +27,7 @@ app.get("/multiply" , (request, response) => {
 })
 
 
+// the end point which takes two number and do division.
 app.get("/divide" , (request, response) => {
     let num1 = parseInt(request.query.num1)
     let num2 = parseInt(request.query.num2)
@@ -29,13 +35,25 @@ app.get("/divide" , (request, response) => {
     response.send(`The multiply of you your numbers is ${multiply}`)
 })
 
-
+// the end point which takes two number and do subtraction.
 app.get("/subtract" , (request, response) => {
     let num1 = parseInt(request.query.num1)
     let num2 = parseInt(request.query.num2)
     let multiply = num1 - num2
     response.send(`The multiply of you your numbers is ${multiply}`)
 })
+
+
+
+
+// Making endpoints using End Point Params
+app.get("/addWithParams/:value1/:value2" , (request, response) => {
+    let num1 = parseInt(request.params.value1)
+    let num2 = parseInt(request.params.value2)
+    let multiply = num1 + num2
+    response.send(`The sum of you your numbers is ${multiply}`)
+})
+
 
 
 
